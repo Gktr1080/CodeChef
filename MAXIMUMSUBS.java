8df818ded863b014557fa69dd2f7148e2062ed1e
Gaurav Katiyar <gauravkatiyar456@gmail.com>
2023-02-28 13:14:15 +0530

    first commit

---
+++ MAXIMUMSUBS.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try {
+		    Scanner sc = new Scanner (System.in);
+		    int T=sc.nextInt();
+		    while(T>0){
+		        int X= sc.nextInt();
+		        System.out.println(2*X);
+		}
+		} 
+		catch(Exception e) {
+		  //  System.out.println("Error");
+		}
+	        
+	        
+	    
+		// your code goes here
+	}
+}
